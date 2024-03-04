b3aa26c0ad183c7736bed6ec118b7d96e5dd44b7
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-03 19:41:10 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+import "./globals.css"
+
+const App = () => {
+  return (
+    <main className="flex h-screen">
+      App
+    </main>
+  )
+}
+
+export default App;
