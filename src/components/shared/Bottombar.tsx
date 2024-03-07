8106972e02db621afa83dbfc812109150fc20234
Rody Huancas <rodyhuancas.04@gmail.com>
2024-03-06 20:32:23 -0500

    Update: align labels

--- src/components/shared/Bottombar.tsx
+++ src/components/shared/Bottombar.tsx
@@ -8,12 +8,10 @@ export const Bottombar = () => {
       {bottombarLinks.map((link) => {
         const isActive = pathname === link.route;
         return (
-
-            <Link to={link.route} key={link.route} className={`${ isActive && 'bg-primary-500 rounded-[10px]' } flex-center flex-col gap-1 p-2 transition`}>
-              <img src={link.imgURL} alt={link.label} width={16} height={16} className={`group-hover:invert-white ${ isActive && "invert-white" }`} />
-              <p className="tiny-medium text-light-2">{ link.label }</p>
-            </Link>
-
+          <Link to={link.route} key={link.route} className={`${   isActive && "bg-primary-500 rounded-[10px]" } flex-center flex-col gap-1 p-2 transition`} >
+            <img src={link.imgURL} alt={link.label} width={16} height={16} className={`group-hover:invert-white ${ isActive && "invert-white" }`} />
+            <p className="tiny-medium text-light-2">{link.label}</p>
+          </Link>
         );
       })}
     </section>
